6b7562877f2f52eec3379ad4afb0d41926c05424
Dina Hafez <dinahafez.8@gmail.com>
2018-07-07 14:54:51 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
   new_collection = []
   i = 0
   while i < collection.length
-    if yield(collection[i]) == true
+    if yield(collection[i])
       new_collection << collection[i]
     end
     i += 1
